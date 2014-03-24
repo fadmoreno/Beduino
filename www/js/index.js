33d939576988cbc3c311d1f0d6fc36de0c29fbb2
fadmoreno <fadmoreno@gmail.com>
2014-03-24 18:39:30 -0300

    resto del refactor

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
 // ControllerBoard functionality
 var board = (function () {
 
-	var dashboard = $('#control-board'),
+	var dashboard = $('#control-board'),getContollerId
 		controllerNameText = $('#controller-name'),
 		controllerPathText = $('#controller-path'),
 		controlNameToAdd = $('#controller-name'),
@@ -11,9 +11,11 @@ var board = (function () {
 		scaleUpButton = $('#scale-up'),
 		scaleDownButton = $('#scale-down'),
 		panelMenuButton = $('#button-panel'),
-		moveMsg = $('#move-msg');
+		moveMsg = $('#move-msg'),
+		editControllerButton = $('#edit-controller-page');
 		
 	var editMode;
+	var controllerObjetId;
 	
 	var privateInit = function(){
 			editMode = false;
@@ -54,12 +56,12 @@ var board = (function () {
 			useCSSTranslation: false,
 			constrainTo: 'parent'
 		}) ;
-		
-		saveButton.addClass('show-controller').removeClass('hide-controller');
-		scaleUpButton.addClass('show-controller').removeClass('hide-controller');
-		scaleDownButton.addClass('show-controller').removeClass('hide-controller');
+		showElement(saveButton);
+		showElement(scaleUpButton);
+		showElement(scaleDownButton);
 		panelMenuButton.addClass('ui-state-disabled');
-		moveMsg.addClass('show-controller').removeClass('hide-controller');
+		showElement(moveMsg);
+		
 	};
 	
 	var editController = function(controllerToEdit, newControlName, newControlPath){
@@ -140,8 +142,7 @@ var board = (function () {
 	};
 	
 	var safeNewController = function(controlSettings){
-			emptyAddControllerInputs();
-			saveButton.removeClass('show-controller').addClass('hide-controller');
+			emptyAddControllerInputs();		
 			scaleUpButton.removeClass('show-controller').addClass('hide-controller');
 			scaleDownButton.removeClass('show-controller').addClass('hide-controller');
 			panelMenuButton.removeClass('ui-state-disabled');
@@ -157,6 +158,54 @@ var board = (function () {
 			*/		
 	};
 	
+	var safeEdition = function(){
+			$('#control-board > a, #control-board > div').each(function(){	
+				var contName = $(this).attr('id');
+				if(contName != undefined || contName != null){
+					contName = contName.split("-");
+					if(contName[0] == "content"){
+						if($(this).hasClass('controller-editing')){
+							var controllerId = getContollerId($(this).attr('id'),function(result) {
+								var controlSettings = {
+										type: $('input:radio[name=radio-choice-v-6]:checked').val(), 
+										name: $('#controller-name').val(),
+										path: $('#controller-path').val(),
+										css: $('#content-'+controlNameToAdd).attr('style'),
+										id: controllerId
+									};
+								});
+							$(this).removeClass('controller-editing');
+						}
+						$(this).removeAttr('data-rel').removeAttr('data-rel').removeAttr('data-rel').removeAttr('href');
+						$(this).removeClass('edit-enable');
+						$(this).next().remove();
+					}
+				}
+			});
+		};
+	
+	var showElement = function(element){
+		element.addClass('show-controller').removeClass('hide-controller');
+	};
+	
+	var prepareControllersToBeEdit = function(){
+		$('#control-board > a, #control-board > div').each(function(){		
+			var contName = $(this).attr('id');
+			contName = contName.split("-");
+			if(contName[0] == "content"){
+				$(this).attr({
+					'data-rel': "popup",
+					'data-transition': "pop",
+					'href': "#popupMenuEdit"
+				});
+				var contCss = $(this).attr('style');
+				var contWidth = $(this).width();
+				var contHeight= $(this).height();
+				var supDiv= '<div class="arduino-action" style="' + contCss + 'display:block;width:'+contWidth+'px;height:'+contHeight+'px;"></div>';
+				$(supDiv).insertAfter($(this));
+			}
+		});
+	};
 	return{
 	
 		init: privateInit,
@@ -203,6 +252,7 @@ var board = (function () {
 			
 			saveButton.on('click',function(event){
 				event.preventDefault();
+				saveButton.removeClass('show-controller').addClass('hide-controller');
 				if(editMode == false){
 					var controlSettings = {
 						type: $('input:radio[name=radio-choice-v-6]:checked').val(), 
@@ -215,37 +265,107 @@ var board = (function () {
 				}
 				else{
 					editMode = false;
-					saveButton.removeClass('show-controller').addClass('hide-controller');
-					$('#button-panel').removeClass('ui-state-disabled');
+					panelMenuButton.removeClass('ui-state-disabled');
 					$('.open-delete-popup').remove();
-
-					$('#control-board > a, #control-board > div').each(function(){	
-						var contName = $(this).attr('id');
+					safeEdition();
 					
-						if(contName != undefined || contName != null){
-							contName = contName.split("-");
-							if(contName[0] == "content"){
-								if($(this).hasClass('controller-editing')){
-									var controllerId = getContollerId($(this).attr('id'),function(result) {
-										console.log('ID success');
-										var controlSettings = {
-											type: $('input:radio[name=radio-choice-v-6]:checked').val(), 
-											name: $('#controller-name').val(),
-											path: $('#controller-path').val(),
-											css: $('#content-'+controlNameToAdd).attr('style'),
-											id: controllerId
-										};
-									});
-									$(this).removeClass('controller-editing');
-								}
-								
-								$(this).removeAttr('data-rel').removeAttr('data-rel').removeAttr('data-rel').removeAttr('href');
-								$(this).removeClass('edit-enable');
-								$(this).next().remove();
-							}
-						}
-					});
 				}
+			});
+			
+			//Borro el borde rojo(indicando requerido) cuando selecciono el campo
+			controllerNameText.focusin(function() {
+				controllerNameText.removeClass('empty');
+			});
+			
+			controllerPathText.focusin(function() {
+				controllerPathText.removeClass('empty');
+			});
+			
+			dashboard.on('click','.arduino-action',function(event){
+				if(editMode == false){
+					getData("","","");
+				}
+				else{
+					event.preventDefault();
+					var popupMenuEdit = $('#popupMenuEdit');
+					popupMenuEdit.popup( "open");
+					if($(this)[0].tagName != "DIV"){
+						$('#open-delete-popup').attr('data-cont-delete',$(this).attr('id'));
+						$(this).addClass('edit-enable');
+					}
+					else{
+						$('#open-delete-popup').attr('data-cont-delete',$(this).prev().attr('id'));
+						$(this).prev().addClass('edit-enable');
+					}
+				}
+			});
+			
+			editControllerButton.on('click',function(event){
+				editMode = true;
+				event.preventDefault();
+				location.hash = "main-page";
+				panelMenuButton.addClass('ui-state-disabled');
+				showElement(saveButton);
+				$( "#menu-panel" ).panel( "close" );
+				
+				prepareControllersToBeEdit();
+			});
+			
+			
+			//Boton delete del menu de edicion
+			$('#open-delete-popup').click(function(event){
+				event.preventDefault();
+				var popupMenuEdit = $('#popupMenuEdit')
+				popupMenuEdit.popup( "close");
+				popupMenuEdit.popup({
+					afterclose: function( event, ui ) {
+						$( "#deleteDialog" ).popup( "open");
+					}
+				});
+				controllerObjetId = $(this).attr('data-cont-delete');
+				
+			});
+			
+			//Delete controller (boton delete)
+			$('#delete-controller-button').click(function(event){
+				event.preventDefault();
+				$('#'+controllerObjetId).remove();
+				$('#deleteDialog').popup( "close" );
+				
+				getContollerIdandDelete(controllerObjetId,function() {
+					console.log('delete success');
+				});
+				
+			});
+			
+			//Delete controller (boton cancel)
+			$('#delete-cancel-button').click(function(event){
+				event.preventDefault();
+				$('#deleteDialog').popup( "close" );
+				$('#control-board').find('.edit-enable').removeClass('edit-enable');
+				
+			});
+			
+			//boton cerrar menu edicion
+			$('#close-button-menu-edit').click(function(event){
+				$('#popupMenuEdit').popup({
+					afterclose: function( event, ui ) {
+						$( "#deleteDialog" ).popup( "close");
+					}
+				});
+				$('#control-board').find('.edit-enable').removeClass('edit-enable');
+			});
+			
+			$('#open-edit-page').on('click',function(event){
+				//editMode = true;
+				event.preventDefault();
+				location.hash = "#output-path-page";
+				var editObjetName = $('#control-board').find('.edit-enable').attr('id');
+				editObjetName = editObjetName.split("-");
+				var editObjetPath = $('#control-board').find('.edit-enable').attr('data-path');
+				
+				$('#controller-name').val(editObjetName[1]);
+				$('#controller-path').val(editObjetPath);
 			});
 		},
 	};
@@ -256,152 +376,3 @@ $(function() {
 	board.init();
 	board.buttonActions();
 });
-$(document).ready(function(){
-	$('#control-board').height($(window).height()-77+'px');
-	/*
-	dbShell = window.openDatabase("controllers", 2, "SimpleNotes", 1000000);
-	console.log('basede datos creda');
-    console.log("db was opened");
-    dbShell.transaction(setupTable,dbErrorHandler,getEntries);
-    console.log("ran setup");
-	*/
-	
-	var controlNameToAdd;
-	var controllerNameText = $('#controller-name');
-	var controllerPathText = $('#controller-path');
-	
-	
-	//Borro el borde rojo(indicando requerido) cuando selecciono el campo
-	controllerNameText.focusin(function() {
-		controllerNameText.removeClass('empty');
-	});
-	
-	controllerPathText.focusin(function() {
-		controllerPathText.removeClass('empty');
-	});
-		
-	
-	$('#control-board').on('click','.arduino-action',function(event){
-		if(editMode == false){
-			getData("","","");
-		}
-		else{
-			event.preventDefault();
-			
-			var popupMenuEdit = $('#popupMenuEdit');
-			popupMenuEdit.popup( "open");
-			if($(this)[0].tagName != "DIV"){
-				$('#open-delete-popup').attr('data-cont-delete',$(this).attr('id'));
-				$(this).addClass('edit-enable');
-			}
-			else{
-				$('#open-delete-popup').attr('data-cont-delete',$(this).prev().attr('id'));
-				$(this).prev().addClass('edit-enable');
-			}
-		}
-	});
-	
-	$('#edit-controller-page').on('click',function(event){
-		editMode = true;
-		event.preventDefault();
-		location.hash = "main-page";
-		$('#button-panel').addClass('ui-state-disabled');
-		$('#save-controller').addClass('show-controller').removeClass('hide-controller');
-		$( "#menu-panel" ).panel( "close" );
-		
-		$('#control-board > a, #control-board > div').each(function(){		
-			var contName = $(this).attr('id');
-			contName = contName.split("-");
-			if(contName[0] == "content"){
-				$(this).attr({
-					'data-rel': "popup",
-					'data-transition': "pop",
-					'href': "#popupMenuEdit"
-				});
-				var contCss = $(this).attr('style');
-				var contWidth = $(this).width();
-				var contHeight= $(this).height();
-				var supDiv= '<div class="arduino-action" style="' + contCss + 'display:block;width:'+contWidth+'px;height:'+contHeight+'px;"></div>';
-				$(supDiv).insertAfter($(this));
-			}
-		});
-		
-	});
-	
-	var controllerObjetId;
-	//Boton delete del menu de edicion
-	$('#open-delete-popup').click(function(event){
-		event.preventDefault();
-		var popupMenuEdit = $('#popupMenuEdit')
-		popupMenuEdit.popup( "close");
-		popupMenuEdit.popup({
-			afterclose: function( event, ui ) {
-				$( "#deleteDialog" ).popup( "open");
-			}
-		});
-		controllerObjetId = $(this).attr('data-cont-delete');
-		console.log('ASDAS');
-	});
-	
-	//Delete controller
-	$('#delete-controller-button').click(function(event){
-		event.preventDefault();
-		$('#'+controllerObjetId).remove();
-		$('#deleteDialog').popup( "close" );
-		
-		getContollerIdandDelete(controllerObjetId,function() {
-			console.log('delete success');
-        });
-		
-	});
-	
-	//Delete controller
-	$('#delete-cancel-button').click(function(event){
-		event.preventDefault();
-		$('#deleteDialog').popup( "close" );
-		$('#control-board').find('.edit-enable').removeClass('edit-enable');
-		
-	});
-	
-	//boton cerrar menu edicion
-	$('#close-button-menu-edit').click(function(event){
-		$('#popupMenuEdit').popup({
-			afterclose: function( event, ui ) {
-				$( "#deleteDialog" ).popup( "close");
-			}
-		});
-		$('#control-board').find('.edit-enable').removeClass('edit-enable');
-	});
-	
-	
-	$('#open-edit-page').on('click',function(event){
-		//editMode = true;
-		event.preventDefault();
-		location.hash = "#output-path-page";
-		var editObjetName = $('#control-board').find('.edit-enable').attr('id');
-		editObjetName = editObjetName.split("-");
-		var editObjetPath = $('#control-board').find('.edit-enable').attr('data-path');
-		
-		$('#controller-name').val(editObjetName[1]);
-		$('#controller-path').val(editObjetPath);
-	});
-	function inputValidate(inputVal,type){
-		var result = true;
-		switch(type){
-			case "empty":
-				if(inputVal != ""){result = true;}else{result = false;}
-			break;
-			case "nameExist":
-				$('#control-board > a, #control-board > div').each(function(){
-					if($(this).attr('id') == "content-"+inputVal.val()){
-						result = false;
-					}
-				});
-			break;
-			default:
-				result = true;
-			}
-		return result;
-	}
-});
-
